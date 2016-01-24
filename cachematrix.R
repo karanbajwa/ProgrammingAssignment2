5bd3be642dc52842d1d02af93e0526dc8dbf66bb
karanbajwa <karanpreetb@gmail.com>
2016-01-24 15:13:12 -0500

    added comments to the first function to explain the usage

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Usage Example
+## m <- matrix(c(1,0,5,2,1,6,3,4,0), nrow=3, ncol=3) # create a invertable matrix of integers
+## cachedMatrix <- makeCacheMatrix(m) # create the cached matrix
+## cacheSolve(cachedMatrix) #returns inverse ---> matrix(c(-24,18,5,20,-15,-4,-5,4,1), nrow=3, ncol=3) 
+## cacheSolve(cachedMatrix) (any subsequent call will return the cached value)
 
-## Write a short comment describing this function
+## This function creates a enhanced "matrix" object that can cache its inverse. 
+## The enhanced "matrix" is a list of functions that provide added functionality to matrix: 
+## set: set current value of the matrix
+## get: get current value of the matrix
+## setinverse: set the value of the inverse matrix
+## getinverse: get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  #initialize the inverse with NULL
   invMatrix <- NULL
   
+  #set the value of 'x' matrix, also reset value of 'invMatrix' to NULL
   set <- function(y){
     x <<- y
     invMatrix <<- NULL
     
   }
 
+  #getter function to return current value of 'x' matrix
   get <- function(){
     x
   } 
   
+  #setter function to set the cached value of 'invMatrix'
   setinverse <- function(inverse){
     invMatrix <<- inverse
   }
   
+  #getter function to return the cached value of 'invMatrix'
   getinverse <- function() {
     invMatrix
   }
   
+  #defines all functions available on the 'x' matrix
   list(
     getinverse = getinverse,
     setinverse = setinverse,
